d4ed098423ce9795285925f65875d83cbbb437dc
Ci-Ci Thomson <public.thomson@gmail.com>
2024-09-22 15:04:00 -0400

    Add search for city and venue

--- app/hello.py
+++ app/hello.py
@@ -7,16 +7,15 @@ import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisa600secretkey#92'
 
+DATABASE_URL = 'postgresql://bcb:uvB4rcXdWk5iWFUQn7ZFwihfJMeGuTlw@dpg-crl2tnqj1k6c73fl0ueg-a.oregon-postgres.render.com/setlist_ldrk'
+
+conn = psycopg2.connect(DATABASE_URL)
 
 @app.route('/')
 def hello():
     # Connect to the database
     #conn = psycopg2.connect(database="setlist", user="postgres",
     #                        password="root", host="localhost", port="5432")
-
-    DATABASE_URL = 'postgresql://bcb:uvB4rcXdWk5iWFUQn7ZFwihfJMeGuTlw@dpg-crl2tnqj1k6c73fl0ueg-a.oregon-postgres.render.com/setlist_ldrk'
-
-    conn = psycopg2.connect(DATABASE_URL)
 
     # create a cursor
     cur = conn.cursor()
@@ -37,13 +36,6 @@ def hello():
 
 @app.route('/shows', methods=['GET'])
 def get_shows():
-    # Connect to the database
-    #conn = psycopg2.connect(database="setlist", user="postgres",
-    #                        password="root", host="localhost", port="5432")
-
-    DATABASE_URL = 'postgresql://bcb:uvB4rcXdWk5iWFUQn7ZFwihfJMeGuTlw@dpg-crl2tnqj1k6c73fl0ueg-a.oregon-postgres.render.com/setlist_ldrk'
-
-    conn = psycopg2.connect(DATABASE_URL)
 
     # create a cursor
     cur = conn.cursor(cursor_factory=RealDictCursor)
@@ -56,25 +48,18 @@ def get_shows():
     print(limit)
     print(offset)
 
-#    main_query = '''select row_to_json(row)
-#from (select show.event_date, v.venue_name, c.city_name, c.state from #artist_show show left join venue v
-#    on show.venue_id = v.id left join city c on v.city_id = c.id LIMIT {0} #OFFSET {1}) row;'''.format(limit, offset)
+    total_shows = getTotalShows()
 
-    total_shows_query = '''select count(*) from artist_show'''
-    count_cursor = conn.cursor()
-    count_cursor.execute(total_shows_query)
-    total_shows = count_cursor.fetchone()[0]
+    #main_query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
+    #on show.venue_id = v.id left join city c on v.city_id = c.id LIMIT {0} OFFSET {1}'''.format(limit, offset)
 
-    main_query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
-    on show.venue_id = v.id left join city c on v.city_id = c.id LIMIT {0} OFFSET {1}'''.format(limit, offset)
-
+    main_query = createMainQuery(None, None, limit, offset)
     cur.execute(main_query)
 
     # Fetch the data
     results = cur.fetchall()
     final_results = {}
     show_list = []
-    show_dict = {}
     final_results['total_shows'] = total_shows
     index = 0
     for row in results:
@@ -90,14 +75,6 @@ def get_shows():
             song_cursor.execute(song_query)
             song_list = song_cursor.fetchall()
             show_list[index]['song_list'] = song_list
-            #final_results['show'] = row
-            #final_results['song_list'] = song_list
-            #print(song_list)
-            #show_songs = song_cursor.fetchall()
-        #song_data = get_associated_songs(artist_show_id)
-        # Add song data to resultset
-        #row_dict['song_data'] = song_data
-        #print(row_dict)
         index = index + 1
     conn.commit()
 
@@ -110,29 +87,108 @@ def get_shows():
     #return jsonify(data, status=200, mimetype='application/json')
     #return make_response(jsonify(data), 200)
 
-def get_associated_songs(artist_show_id):
-    DATABASE_URL = 'postgresql://bcb:uvB4rcXdWk5iWFUQn7ZFwihfJMeGuTlw@dpg-crl2tnqj1k6c73fl0ueg-a.oregon-postgres.render.com/setlist_ldrk'
+@app.route('/search', methods=['GET'])
+def search_shows():
 
-    conn = psycopg2.connect(DATABASE_URL)
+    page = request.args.get('page', 1, type=int)
+    limit = request.args.get('limit', 20, type=int)
+    term = request.args.get('term')
+    action = request.args.get('action')
+    offset = (limit * page) - limit;
 
-    # create a cursor
+    if action == 'song':
+        searchResults = searchBySong(term, action, limit, offset)
+    else:
+        searchResults = searchByTerm(term, action, limit, offset)
+
+
+    return searchResults
+
+def searchByTerm(term, action, limit, offset):
+    main_query = createMainQuery(term, action, limit, offset)
+
+    # Execute first query
     cur = conn.cursor(cursor_factory=RealDictCursor)
-
-    song_query = '''select ss.song_name, ss.encore, a.artist_name from song_show ss
- left join song s on ss.song_name=s.song_name
- left join artist a on s.cover_artist_id=a.mbid
- where song_show_id='{0}' '''.format(artist_show_id)
-
-    cur.execute(song_query)
+    cur.execute(main_query)
 
     # Fetch the data
     results = cur.fetchall()
-    #print('song results for id ')
-    #print(artist_show_id)
-    #print(results)
+    final_results = {}
+    show_list = []
 
-    # close the cursor and connection
-    cur.close()
-    conn.close()
-    return results
+    # Scroll through list of shows and add songs
+    index = 0
+    for row in results:
+        # Append main show information
+        show_list.append(dict(row))
+        # Get the songs
+        artist_show_id = row['artist_show_id']
+        with conn.cursor(cursor_factory=RealDictCursor) as song_cursor:
+            song_query = createSongQuery(term, action, artist_show_id)
+            song_cursor.execute(song_query)
+            song_list = song_cursor.fetchall()
+            show_list[index]['song_list'] = song_list
+        index = index + 1
+
+    final_results['show_list'] = show_list
+    #print(final_results)
+    return final_results
+
+def searchBySong(term, action, limit, offset):
+    song_query = createSongQuery(term, action, None)
+
+    # create a cursor
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute(song_query)
+    # Fetch the data
+    results = cur.fetchall()
+    final_results = {}
+    show_list = []
+    # Scroll through list of songs and add shows
+    index = 0
+    print("Search by Song")
+    for row in results:
+       print(row['artist_show_id'])
+       show_query = createShowQuery(row['artist_show_id'])
+    return final_results
+
+def createMainQuery(term, action, limit, offset):
+    if action == 'venue':
+        query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
+        on show.venue_id = v.id left join city c on v.city_id = c.id
+        where v.venue_name like '%{0}%' LIMIT {1} OFFSET {2}'''.format(term,limit, offset)
+    elif action == 'city':
+        query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
+        on show.venue_id = v.id left join city c on v.city_id = c.id
+        where c.city_name like '%{0}%' LIMIT {1} OFFSET {2}'''.format(term,limit, offset)
+    else:
+        query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
+        on show.venue_id = v.id left join city c on v.city_id = c.id LIMIT {0} OFFSET {1}'''.format(limit, offset)
+    return query
+
+def createSongQuery(term, action, artist_show_id):
+    if action == 'song':
+        query = '''select show.artist_show_id, ss.song_show_id, ss.song_name, ss.encore, a.artist_name from song_show ss
+        left join song s on ss.song_name=s.song_name
+        left join artist a on s.cover_artist_id=a.mbid
+        left join artist_show show on show.artist_show_id=ss.song_show_id
+        where ss.song_name like '%{0}%' '''.format(term)
+    else:
+        query = '''select ss.song_name, ss.encore, a.artist_name from song_show ss
+        left join song s on ss.song_name=s.song_name
+        left join artist a on s.cover_artist_id=a.mbid
+        where song_show_id='{0}' '''.format(artist_show_id)
+    return query
+
+def createShowQuery(artist_show_id):
+    query = '''select show.artist_show_id, show.event_date, v.venue_name,  c.city_name, c.state from artist_show show left join venue v
+    on show.venue_id = v.id left join city c on v.city_id = c.id where show.artist_show_id = '{0}' '''.format(artist_show_id)
+
+    return query
+
+def getTotalShows():
+    total_shows_query = '''select count(*) from artist_show'''
+    count_cursor = conn.cursor()
+    count_cursor.execute(total_shows_query)
+    return count_cursor.fetchone()[0]
 
